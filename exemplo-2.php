f0a706e6499e503c4a3be5ef48537eeed382e9fd
Mariana <mdxavier@live.com>
2020-08-24 20:37:22 -0300

    .

--- exemplo-2.php
+++ exemplo-2.php
@@ -15,7 +15,7 @@ echo "<br/>";
 
 unset($nome1);
 
-if(isset($nome1)); //através disso já estou entendendo o crud e vou estudá-lo concomittantemente ao curso da udemy
+if(isset($nome1)); //através disso já estou entendendo o crud e vou estudá-lo concomitantemente ao curso da udemy
  {
 
 echo $nome1;
